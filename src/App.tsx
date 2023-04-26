7015bbedf81f7d29005d74f0fe288e1f4bde8b68
luiznaiper <luiz_naiper@hotmail.com>
2023-04-26 07:38:27 -0300

    :construction: Add nanoid and update setTodo state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
+import { Todo } from './model'
+import { nanoid } from 'nanoid'
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
 
-  console.log(todo)
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo) {
+      setTodos([...todos, { id: nanoid(), todo: todo, isDone: false }])
+      setTodo('')
+    }
+  }
+
+  console.log(todos)
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   )
 }
